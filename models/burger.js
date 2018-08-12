fd5a7143637592432ccef5c851bf35aad4b2c553
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-08-12 15:43:35 -0400

    created burger model

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require("orm");
+
+var burger = {
+	select: function(cb) {
+		orm.selectAll(function(data) {
+			cb(data);
+		});
+	},
+
+	insert: function(burger, cb) {
+		orm.insertOne(burger, function(data) {
+			cb(data);
+		});
+	},
+
+	update: function(burger, cb) {
+		orm.updateOne(burger, function(data) {
+			cb(data);
+		});
+	}
+};
+
+module.exports = burger;
