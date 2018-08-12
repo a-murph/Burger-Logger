46b1462a75448dacf951c113f12d8bf24f7dfd1f
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-08-12 15:18:43 -0400

    created orm.js

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+var db = require("./connection.js");
+
+var orm = {
+	selectAll: function() {
+
+	},
+
+	insertOne: function() {
+
+	},
+
+	updateOne: function() {
+
+	}
+}
+
+module.exports = orm;
