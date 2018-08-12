94f6e2e96f60faf223c55e92c12b75a0ceb36cbc
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-08-12 15:29:10 -0400

    added orm logic

--- config/orm.js
+++ config/orm.js
@@ -2,15 +2,45 @@ var db = require("./connection.js");
 
 var orm = {
 	selectAll: function() {
+		db.query(
+			"SELECT * FROM burgers",
+			function(err, data) {
+				if (err) throw err;
 
+				return data;
+			}
+		);
 	},
 
-	insertOne: function() {
+	insertOne: function(newBurger) {
+		db.query(
+			"INSERT INTO burgers (burger_name) VALUES (?)",
+			{
+				burger_name: newBurger
+			},
+			function(err, data) {
+				if (err) throw err;
 
+				return data;
+			}
+		);
 	},
 
-	updateOne: function() {
+	updateOne: function(burger) {
+		db.query(
+			"UPDATE burgers SET ? WHERE ?",
+			{
+				burger_name: burger.name,
+				eaten: burger.eaten
+			}, {
+				id: burger.id
+			},
+			function(err, data) {
+				if (err) throw err;
 
+				return data;
+			}
+		);
 	}
 }
 
